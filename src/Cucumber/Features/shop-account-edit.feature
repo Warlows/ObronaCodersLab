b1ad325e3418adf69d5396c740345301dcd662d0
Paweł Drohomirecki <jin4ster@gmail.com>
2023-02-27 02:18:39 +0100

    steps5 done

--- src/Cucumber/Features/shop-account-edit.feature
+++ src/Cucumber/Features/shop-account-edit.feature
@@ -7,15 +7,15 @@ Feature: User Login it the shop and edit account
     When I sign in
     And I login using "jin4ster.pr1@gmail.com" and "terakonia84"
     And I open my address page
-    And I enter new address "<Alias>", "<Address>", "<City>", "<Postal code>","<Country>", "<Phone>"
-   Then I check the correct data in the address
-#   And I delete new address
-#   And I can see new address delete*
+    And I enter new address <Alias>, <Address>, <City>, <Postal_code>,<Country>, <Phone>
+   Then I can see success message whit text "Your data has been created."
+   And I delete new address
+   And I can see new address delete*
 
     Examples:
-      |Alias     |Address   |City   |Postal code |Country     |Phone         |
-      |Alias01   |Dog       |Szczec |71-830      |Pol         |555-444-333   |
-      |Alias02   |Fog       |Starg  |70-830      |Pol         |333-444-555   |
+      |Alias     |Address   |City   |Postal_code |Country         |Phone         |
+      |Alias01   |Dog       |Szczec |71-830      |United Kingdom  |555-444-333   |
+
 
 
 
--- src/test/java/pl/shop/ShopPageObjectSteps.java
+++ src/test/java/pl/shop/ShopPageObjectSteps.java
@@ -2,6 +2,7 @@ package pl.shop;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.netty.buffer.ByteBufUtil;
 import org.openqa.selenium.By;
@@ -11,6 +12,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ShopPageObjectSteps {
     private WebDriver driver;
@@ -34,7 +38,7 @@ public class ShopPageObjectSteps {
    }
 
 
-    @And("I login using \"(.+)\" and \"(.+)\"$")
+    @And("I login using (.+) and (.+)$")
     public void iLoginUsingAnd(String email, String password) {
         ShopLoginPage loginPage = new ShopLoginPage(driver);
         loginPage.loginAs("jin4ster.pr1@gmail.com", "terakonia84");
@@ -42,7 +46,37 @@ public class ShopPageObjectSteps {
 
     @And("I open my address page")
     public void iOpenMyAddressPage() {
-        driver.findElement(By.className("link-item")).click();
+        driver.findElement(By.id("address-link")).click();
+    }
+
+    @And("I enter new address (.+),(.+),(.+),(.+),(.+),(.+)$")
+    public void iEnterNewAddress(String Alias, String Address, String City, String Postal_code, String Country, String Phone) {
+        driver.findElement(By.name("alias")).sendKeys(Alias);
+        driver.findElement(By.name("address1")).sendKeys(Address);
+        driver.findElement(By.name("city")).sendKeys(City);
+        driver.findElement(By.name("postcode")).sendKeys(Postal_code);
+        driver.findElement(By.name("id_country")).sendKeys(Country);
+        driver.findElement(By.name("phone")).sendKeys(Phone);
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/form/footer/button")).click();
+
+    }
+
+
+    @Then("I can see success message whit text {string}")
+    public void iCanSeeSuccessMessageWhitText(String msgText) {
+        WebElement alert = driver.findElement(By.cssSelector(".alert.alert-success"));
+        assertTrue(alert.isDisplayed());
+        assertEquals(msgText, alert.getText());
+    }
+
+    @And("I delete new address")
+    public void iDeleteNewAddress() {
+        driver.findElement(By.xpath("//*[@id=\"address-32322\"]/div[2]/a[2]/span")).click();
+    }
+
+    @And("I can see new address delete*")
+    public void iCanSeeNewAddressDelete() {
+        driver.findElement(By.xpath("//*[@id=\"content\"]"));
     }
 }
 
